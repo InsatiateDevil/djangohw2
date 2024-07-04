ff5e312b1b47a5c0079b0c8d49c9fb1d29e71e51
Insatiate Devil <ILIDANUM@yandex.ru>
2024-07-04 23:29:23 +0300

    homework 22.1 with bug - command del_and_fill.py

--- catalog/management/commands/del_and_fill.py
+++ catalog/management/commands/del_and_fill.py
@@ -3,7 +3,7 @@ import os
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
 from config.settings import BASE_DIR
 
 file_path = os.path.join(BASE_DIR, 'db.json')
@@ -23,12 +23,13 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-
         # Создайте списки для хранения объектов
         product_for_create = []
         category_for_create = []
+        version_for_create = []
 
-        # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
+        # Обходим все значения категорий из фиктсуры для получения информации
+        # об одном объекте
         for category in Command.json_read(file_path):
             if category['model'] == 'catalog.category':
                 category_for_create.append(
@@ -53,5 +54,16 @@ class Command(BaseCommand):
                             price=product['fields']['price'])
                 )
 
-        # Создаем объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
+
+        for version in Command.json_read(file_path):
+            if version['model'] == 'catalog.version':
+                version_for_create.append(
+                    Version(pk=version['pk'], product=Product.objects.get(
+                        pk=version['fields']['product']),
+                            version_number=version['fields']['version_number'],
+                            version_name=version['fields']['version_name'],
+                            is_active=version['fields']['is_active'])
+                )
+
+        Version.objects.bulk_create(version_for_create)
